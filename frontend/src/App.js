9675d1daa282de7c41dd8b83a3aad22e4f187f2f
Shadan Kazmi <125872053+ShadanKazmi@users.noreply.github.com>
2025-02-17 20:53:53 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import Favourites from './pages/Favourites';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Cookies from 'js-cookie';
+
 
 function App() {
-
+    const userId = Cookies.get("userId");
   return (
     <div className='flex'>
       <Sidebar  />
       <div className="flex-1 p-4">
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/favourites" element={userId ? <Favourites /> : <Navigate to="/" />}/>
         </Routes>
       </div>
     </div>
